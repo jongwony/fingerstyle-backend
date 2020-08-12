1f29317e8f90e52243f506e16baf3db1ecb88970
jongwony <lastone9182@gmail.com>
2020-08-12 12:08:59 +0900

    session hack

--- chalicelib/instagram.py
+++ chalicelib/instagram.py
@@ -22,17 +22,31 @@ class InstagramPublicSession(Session):
         super().__init__()
 
     def get(self, *args, **kwargs):
-        return super().get(f'https://instagram.com/jongwony_/', params={'__a': 1})
+        headers = {
+            'authority': 'www.instagram.com',
+            'upgrade-insecure-requests': '1',
+            'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36 Edg/84.0.522.49',
+            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+            'sec-fetch-site': 'none',
+            'sec-fetch-mode': 'navigate',
+            'sec-fetch-user': '?1',
+            'sec-fetch-dest': 'document',
+            'accept-language': 'ko,en;q=0.9,en-US;q=0.8',
+        }
+        cookies = {'sessionid': '3872333657%3ArsNi7Wg6jpb689%3A17'}
+        params = {'__a': 1}
+        return super().get(f'https://instagram.com/jongwony_/', params=params, headers=headers, cookies=cookies)
 
 
 def carousel_sync():
     session = InstagramPublicSession()
+    response = session.get()
+    print(response.content)
     images = jmespath.search(
         'graphql.user.edge_owner_to_timeline_media.edges[].node | '
         '[].{"img-src": display_url, caption: edge_media_to_caption.edges[0].node.text}',
-        session.get().json()
+        response.json()
     )
-    print(images)
     return [image for image in images if '#guitar' in image['caption']]
 
 
